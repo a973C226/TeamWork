64af3981ef24535e9a14a802fc0e2c2ac6ae5265
Rodion Ibraev <roriibra@gmail.com>
2023-11-22 00:05:00 +0300

    feature: Создан шаблон для главной страницы

--- Project/project/urls.py
+++ Project/project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('teamwork_app.urls'))
+    path('', include('teamwork_app.urls'))
 ]
--- Project/teamwork_app/urls.py
+++ Project/teamwork_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from .views import RegisterView, AuthView
 
 urlpatterns = [
         path("register/", RegisterView.as_view(), name="registration"),
-        path("login/", AuthView.as_view(), name="login")
+        path("login/", AuthView.as_view(), name="login"),
+        path("", TemplateView.as_view(template_name='mainScreen.html'), name="main-screen"),
 ]
